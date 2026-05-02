b2d2c735be4c281292c058c79913d2b78208a608
rdevang71 <rdevang71@gmail.com>
2026-05-02 10:24:24 +0530

    added deployed backend URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const base = import.meta.env.VITE_API_BASE || '/api';
+const api = axios.create({ baseURL: base });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('pf_token');
